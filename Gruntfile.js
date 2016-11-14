b5a9b48826d352a6d299585a9a35d5ab67788286
Phil Baker <p.baker@replyltd.co.uk>
2016-11-14 16:20:23 +0000

    Added emojis 1 to 40

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       options: {
-      sourceMap: true
+      sourceMap: false
     },
       dist: {
         files: {
