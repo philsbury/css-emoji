f4eebb17fadb9117214ed7053f00a55491801d4e
Phil Baker <p.baker@replyltd.co.uk>
2016-11-15 12:15:51 +0000

    Added all icons, created basic example page

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
-        push: true,
+        push: false,
         pushTo: 'origin master',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
         globalReplace: false,
